3eaaef697080cff4fd105ad707cadcb4a8e4fd22
SujalMhr <ssuuzzaall.mhr8@gmail.com>
2026-02-09 15:44:39 +0545

    Initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,52 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
+
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    }),
+  );
+}
+
+//middlewaqrew
+app.use(express.json()); //this middleware get access to req.body, our simple custom middleware
+
+app.use(rateLimiter);
+
+app.use((req, res, next) => {
+  console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
+  next();
+});
+
+app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  //different middleware from node
+
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+console.log("NODE_ENV:", process.env.NODE_ENV);
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server started on PORT:", PORT);
+  });
+});
